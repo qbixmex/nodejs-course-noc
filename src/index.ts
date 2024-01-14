e481b3e1d1aad1bce94ce6cc3a6dfc44fb72b455
Daniel González Briseño <qbixmex@gmail.com>
2024-01-14 09:48:23 -0800

    3.5.0-mongo-create-and-read

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,37 @@
 import { envs } from "./config/plugins/envs.plugin";
-import { MongoDatabase } from "./data/mongo";
+import { LogModel, MongoDatabase } from "./data/mongo";
 import Server from "./presentation/server";
 
-const main = () => {
-  Server.start();
-}
-
-(async () => {
-
+const main = async () => {
   await MongoDatabase.connect({
     mongoURL : envs.MONGO_URL,
     dbName   : envs.MONGO_DB_NAME,
   });
 
-  main()
-})();
+  //* Create Data
+
+  // const newLog = await LogModel.create({
+  //   message: 'Test Message 4 from Mongo',
+  //   origin : 'index.ts',
+  //   level  : 'LOW',
+  // });
+  // await newLog.save();
+  // console.log('New Log:', newLog);
+
+  //* Read Data
+  // const logs = await LogModel.find();
+  // console.log(logs);
+
+  //* Find by ID
+  // const log = await LogModel.findById('65a41bffbefd69126d887f22');
+  // console.log(log);
+
+  //* Filter Data
+  // const highLogs = await LogModel.find({ level: 'HIGH' });
+  // console.log("Count:", highLogs.length);
+  // console.log(highLogs);
+
+  // Server.start();
+}
+
+(() => main())();
