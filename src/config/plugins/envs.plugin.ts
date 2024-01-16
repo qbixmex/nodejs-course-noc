5faae5c30e4d6fdc69584a8fb05f8bed96945202
Daniel González Briseño <qbixmex@gmail.com>
2024-01-16 10:19:38 -0800

    3.8.0-postgres-installation

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -12,4 +12,8 @@ export const envs = {
   MONGO_DB_NAME: env.get("MONGO_DB_NAME").required().asString(),
   MONGO_USER: env.get("MONGO_USER").required().asString(),
   MONGO_PASSWORD: env.get("MONGO_PASSWORD").required().asString(),
+  POSTGRES_URL: env.get("POSTGRES_URL").required().asString(),
+  POSTGRES_DB: env.get("POSTGRES_DB").required().asString(),
+  POSTGRES_USER: env.get("POSTGRES_USER").required().asString(),
+  POSTGRES_PASSWORD: env.get("POSTGRES_PASSWORD").required().asString(),
 };
